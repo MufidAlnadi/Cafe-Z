bfdf015e43be171610928e049bf209d0a7d08302
MufidAlnadi <mufidhussein@gmail.com>
2023-03-20 14:43:36 +0300

    task

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 let name = prompt("Please enter your name");
-let gender = prompt("Please enter your gender (male or female):");
 
+let gender = "";
+while  (gender !== 'male' && gender !== 'female') {
+ gender = prompt("Please enter your gender (male or female): ");
+
+} 
+  
 if (gender === "male") {
   alert("Hello Mr " + name);
 } else if (gender === "female") {
   alert("Hello Ms " + name);
-} else {
-  alert("Hello " + name);
-}
+} 
+
 let drink = prompt("do you want a hot or a cold drink?")
 let drinkName= prompt("name of the drink")
 alert("your drink is getting prepared")
+let arr = [ name , gender  , drink , drinkName]
 
-console.log(name + " ordered a " + drink + " "+drinkName+ ".");
+for (let i = 0 ; i < arr.length; i++){
+  console.log(arr[i])
+}
+
