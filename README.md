d0deb78ea5a3a5b57622a6fe9accfd65a7525a49
MufidAlnadi <mufidhussein@gmail.com>
2023-03-20 15:37:26 +0300

    task

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ### Welcome to the Café Shop Website repository! This is a web application designed for a café shop that allows customers to view the café menu, order food and drinks, and make reservations.
 
+## website
+[live](https://mufidalnadi.github.io/Cafe-Z/)
+
 ## tools
 - VScode
 - html 
--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 let name = prompt("Please enter your name");
 
-let gender = findout();
-function findout(){
-let gender;
-  while  (gender !== 'male' && gender !== 'female') {
+let gender = "";
+while  (gender !== 'male' && gender !== 'female') {
  gender = prompt("Please enter your gender (male or female): ");
-
 } 
-  
+
 if (gender === "male") {
   alert("Hello Mr " + name);
 } else if (gender === "female") {
   alert("Hello Ms " + name);
-} }
+} 
 
 let drink = prompt("do you want a hot or a cold drink?")
 let drinkName= prompt("name of the drink")
@@ -23,4 +20,3 @@ for (let i = 0 ; i < arr.length; i++){
   console.log(arr[i])
 }
 
-// test2
